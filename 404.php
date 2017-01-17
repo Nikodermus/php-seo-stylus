ce67db89d0622a22014dda78ba4cf4e9a398b747
Nicolas M. Pardo <nicolasm@nikodermus.media>
2017-01-16 23:20:40 -0500

    Unnecessary php openings removed

--- 404.php
+++ 404.php
@@ -2,17 +2,14 @@
 $title = 'Oops! You are not connected to internet' ; 
 $description = 'Well what can I say, this is an Offline Version for the site you are visiting, please, if this message is not enough for the information you are looking for try to connect to internet and check again' ; 
 $keywords = 'offline' ; 
-      ?>
-    <?php include 'lib/head.php'; ?>
-        <?php include 'lib/nav.php'; ?>
+    include 'lib/head.php'; include 'lib/nav.php'; ?>
 
-            <section>
-                <article>
-                    <h1>you are offline now! congrats</h1>
-                    <img src="img/logo.svg" alt="">
+    <section>
+        <article>
+            <h1>you are offline now! congrats</h1>
+            <img src="img/logo.svg" alt="">
 
-                </article>
-            </section>
+        </article>
+    </section>
 
-            <?php include 'lib/footer.php'; ?>
-                <?php include 'lib/scripts.php'; ?>
+    <?php include 'lib/footer.php'; include 'lib/scripts.php'; ?>
--- blank.php
+++ blank.php
@@ -3,22 +3,19 @@
 $title = 'Title' ; 
 $description = 'Description' ; 
 $keywords = 'keywords, divided, by, commas' ; 
-      ?>
-    <?php include 'lib/head.php'; ?>
-        <?php include 'lib/nav.php'; ?>
+    include 'lib/head.php';  include 'lib/nav.php'; ?>
 
-            <section>
-                <article>
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+    <section>
+        <article>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
 
-                            </div>
-                        </div>
                     </div>
+                </div>
+            </div>
 
-                </article>
-            </section>
+        </article>
+    </section>
 
-            <?php include 'lib/footer.php'; ?>
-                <?php include 'lib/scripts.php'; ?>
+    <?php include 'lib/footer.php'; include 'lib/scripts.php'; ?>
