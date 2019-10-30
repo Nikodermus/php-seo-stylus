ab73e51d52f9d282af51205ba4a126b0aeef56a9
Nicolás M. Pardo <nicolasm@nikodermus.media>
2019-10-30 13:15:17 -0500

    feat(linter/formatter): include eslint and prettier

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     retinajs();
-    console.log("ready");
+    console.log('ready');
 });
